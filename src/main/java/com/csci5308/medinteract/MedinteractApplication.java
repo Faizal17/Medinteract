ec8b6546ac856e1ab3bafefca3675a84cf76fd10
tapanpatel <tapan.patel@dal.ca>
2023-02-01 18:08:40 -0400

    initialise repo

---
+++ src/main/java/com/csci5308/medinteract/MedinteractApplication.java
@@ -0,0 +1,13 @@
+package com.csci5308.medinteract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedinteractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedinteractApplication.class, args);
+	}
+
+}
