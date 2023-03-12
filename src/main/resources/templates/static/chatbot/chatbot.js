cf3ae27eb9683ea48fe83607f0a5df992a81545f
tapanpatel <tapan.patel@dal.ca>
2023-03-11 23:44:50 -0400

    Added the frontend for Chatbot

---
+++ src/main/resources/templates/static/chatbot/chatbot.js
@@ -0,0 +1,237 @@
+var sendForm = document.querySelector('#chatform'),
+    textInput = document.querySelector('.chatbox'),
+    chatList = document.querySelector('.chatlist'),
+    userBubble = document.querySelectorAll('.userInput'),
+    botBubble = document.querySelectorAll('.bot__output'),
+    animateBotBubble = document.querySelectorAll('.bot__input--animation'),
+    overview = document.querySelector('.chatbot__overview'),
+    hasCorrectInput,
+    imgLoader = false,
+    animationCounter = 1,
+    animationBubbleDelay = 600,
+    input,
+    previousInput,
+    isReaction = false,
+    unkwnCommReaction = "I didn't quite get that.",
+    chatbotButton = document.querySelector(".submit-button")
+
+var createappointment = document.getElementById("createappointment");
+createappointment.addEventListener("click", function(event){
+
+
+});
+
+
+
+sendForm.onkeydown = function(e){
+  if(e.keyCode == 13){
+    e.preventDefault();
+
+    //No mix ups with upper and lowercases
+    var input = textInput.value.toLowerCase();
+
+    //Empty textarea fix
+    if(input.length > 0) {
+      createBubble(input)
+    }
+  }
+};
+
+sendForm.addEventListener('submit', function(e) {
+  //so form doesnt submit page (no page refresh)
+  e.preventDefault();
+
+  //No mix ups with upper and lowercases
+  var input = textInput.value.toLowerCase();
+
+  //Empty textarea fix
+  if(input.length > 0) {
+    createBubble(input)
+  }
+}) //end of eventlistener
+
+var createBubble = function(input) {
+  //create input bubble
+  var chatBubble = document.createElement('li');
+  chatBubble.classList.add('userInput');
+
+  //adds input of textarea to chatbubble list item
+  chatBubble.innerHTML = input;
+
+  //adds chatBubble to chatlist
+  chatList.appendChild(chatBubble)
+
+  checkInput(input);
+}
+
+var checkInput = function(input) {
+  hasCorrectInput = false;
+  isReaction = false;
+  //Checks all text values in possibleInput
+  for(var textVal in possibleInput){
+    //If user reacts with "yes" and the previous input was in textVal
+    if(input == "yes" || input.indexOf("yes") >= 0){
+      if(previousInput == textVal) {
+        console.log("sausigheid");
+
+        isReaction = true;
+        hasCorrectInput = true;
+        botResponse(textVal);
+      }
+    }
+    if(input == "no" && previousInput == textVal){
+      unkwnCommReaction = "For a list of commands type: Commands";
+      unknownCommand("I'm sorry to hear that :(")
+      unknownCommand(unkwnCommReaction);
+      hasCorrectInput = true;
+    }
+    //Is a word of the input also in possibleInput object?
+    if(input == textVal || input.indexOf(textVal) >=0 && isReaction == false){
+      console.log("succes");
+      hasCorrectInput = true;
+      botResponse(textVal);
+    }
+  }
+  //When input is not in possibleInput
+  if(hasCorrectInput == false){
+    console.log("failed");
+    unknownCommand(unkwnCommReaction);
+    hasCorrectInput = true;
+  }
+}
+
+// debugger;
+
+function botResponse(textVal) {
+  //sets previous input to that what was called
+  // previousInput = input;
+
+  //create response bubble
+  var userBubble = document.createElement('li');
+  userBubble.classList.add('bot__output');
+
+  if(isReaction == true){
+    if (typeof reactionInput[textVal] === "function") {
+      //adds input of textarea to chatbubble list item
+      userBubble.innerHTML = reactionInput[textVal]();
+    } else {
+      userBubble.innerHTML = reactionInput[textVal];
+    }
+  }
+
+  if(isReaction == false){
+    //Is the command a function?
+    if (typeof possibleInput[textVal] === "function") {
+      // console.log(possibleInput[textVal] +" is a function");
+      //adds input of textarea to chatbubble list item
+      userBubble.innerHTML = possibleInput[textVal]();
+    } else {
+      userBubble.innerHTML = possibleInput[textVal];
+    }
+  }
+  //add list item to chatlist
+  chatList.appendChild(userBubble) //adds chatBubble to chatlist
+
+  // reset text area input
+  textInput.value = "";
+}
+
+function unknownCommand(unkwnCommReaction) {
+  // animationCounter = 1;
+
+  //create response bubble
+  var failedResponse = document.createElement('li');
+
+  failedResponse.classList.add('bot__output');
+  failedResponse.classList.add('bot__output--failed');
+
+  //Add text to failedResponse
+  failedResponse.innerHTML = unkwnCommReaction; //adds input of textarea to chatbubble list item
+
+  //add list item to chatlist
+  chatList.appendChild(failedResponse) //adds chatBubble to chatlist
+
+  animateBotOutput();
+
+  // reset text area input
+  textInput.value = "";
+
+  //Sets chatlist scroll to bottom
+  chatList.scrollTop = chatList.scrollHeight;
+
+  animationCounter = 1;
+}
+
+function responseText(e) {
+
+  var response = document.createElement('li');
+
+  response.classList.add('bot__output');
+
+  //Adds whatever is given to responseText() to response bubble
+  response.innerHTML = e;
+
+  chatList.appendChild(response);
+
+  animateBotOutput();
+
+  console.log(response.clientHeight);
+
+  //Sets chatlist scroll to bottom
+  setTimeout(function(){
+    chatList.scrollTop = chatList.scrollHeight;
+    console.log(response.clientHeight);
+  }, 0)
+}
+
+
+
+//change to SCSS loop
+function animateBotOutput() {
+  chatList.lastElementChild.style.animationDelay= (animationCounter * animationBubbleDelay)+"ms";
+  animationCounter++;
+  chatList.lastElementChild.style.animationPlayState = "running";
+}
+
+function commandReset(e){
+  animationCounter = 1;
+  previousInput = Object.keys(possibleInput)[e];
+}
+
+
+var possibleInput = {
+
+  "hi" : function(){
+    responseText("Hello, How can i help you?");
+
+    commandReset(2);
+    return
+  },
+
+
+  "humidity" : function(){
+    var weather= new XMLHttpRequest();
+    weather.open("GET", "https://api.thingspeak.com/channels/725914/feeds.json?api_key=R8UXP1FFMCLN5G4E", false);
+    weather.send(null);
+    var r =JSON.parse(weather.response);
+    celcius = Math.round(r.feeds[r.channel.last_entry_id-1].field1);
+    coverage = r.feeds[r.channel.last_entry_id-1].field2;
+    responseText("The humidity is "+coverage);
+    commandReset(2);
+    return
+  }
+
+  // ,
+  // "contact" : function(){
+  //   responseText("email: <a href='mailto:meesrutten@gmail.com?Subject=Hello%20Mees' target='_top'>send me a message</a>");
+  //   responseText("Twitter: <a href='https://twitter.com/meesrttn'>@MeesRttn</a>");
+  //   commandReset(7);
+  //   return
+  // }
+  ,
+  "rick roll" : function(){
+    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+  },
+  // work experience
+}
+
