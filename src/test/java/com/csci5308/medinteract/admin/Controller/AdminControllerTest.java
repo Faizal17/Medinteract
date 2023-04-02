30c9241f6dfea669358af8a640d22577b0fa366b
tapanpatel <tapan.patel@dal.ca>
2023-04-02 01:41:48 -0300

    added integration test for Admin with 100% code coverage

--- src/test/java/com/csci5308/medinteract/admin/Controller/AdminControllerTest.java
+++ src/test/java/com/csci5308/medinteract/admin/Controller/AdminControllerTest.java
@@ -17,16 +17,28 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class AdminControllerTest {
 
+    static String failedMsg = "Admin log-in Failed!";
+    static String successMsg = "User logged in Successfully!";
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    void login() throws Exception {
-            mockMvc.perform(post("http://localhost:6969/admin/login")
+    void loginSuccess() throws Exception {
+            mockMvc.perform(post("/admin/login")
                             .contentType(MediaType.APPLICATION_JSON)
                             .content("{ \"adminEmail\": \"admin\",\"adminPassword\": \"Group27\" }"))
                     .andExpect(status().isOk())
-                    .andExpect(jsonPath("$.msg").value("User logged in Successfully!"))
+                    .andExpect(jsonPath("$.msg").value(successMsg))
                     .andExpect(jsonPath("$.isError").value("false"));
+    }
+
+    @Test
+    void loginFailed() throws Exception {
+        mockMvc.perform(post("http://localhost:6969/admin/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{ \"adminEmail\": \"admin\",\"adminPassword\": \"Group7\" }"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.msg").value(failedMsg))
+                .andExpect(jsonPath("$.isError").value("true"));
     }
 }
