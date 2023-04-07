84e479f62e313976635a6e4ac2808b3e914bb952
Mohammedfaizal Maulvi <mh795616@dal.ca>
2023-04-07 17:36:32 -0300

    added health check api and test case

---
+++ src/main/java/com/csci5308/medinteract/utilities/Response/ResponseController.java
@@ -0,0 +1,15 @@
+package com.csci5308.medinteract.utilities.Response;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/server")
+public class ResponseController {
+    @GetMapping("/healthCheck")
+    public ResponseEntity healthCheck() {
+        Response res = new Response("", false, "Server is running on port 6969");
+        return new ResponseEntity<>(res.getResponse(), HttpStatus.OK);
+    }
+}
